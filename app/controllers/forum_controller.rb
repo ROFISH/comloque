784e3a7f6f55d8967f3e9d4be9fcfaee4b36c402
Ryan Alyea <ryan@fangamer.com>
2014-05-07 00:56:51 -0700

    Topiclist and requiring a forum

--- app/controllers/forum_controller.rb
+++ app/controllers/forum_controller.rb
@@ -1,5 +1,17 @@
 class ForumController < PublicController
+  before_filter :require_forum, :only=>[:topiclist]
   def index
     @forums = Forum.all.to_a
+  end
+
+  def topiclist
+  end
+
+private
+  def require_forum
+    permalink = Permalink.find_by_name_and_thang_type(params[:forum],"Forum")
+    raise ActiveRecord::RecordNotFound if permalink.blank?
+
+    @forum = permalink.thang
   end
 end
