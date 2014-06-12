cb6a631a6f6003fafdf1bb54be9d9dd018a28675
Ryan Alyea <ryan@fangamer.com>
2014-06-11 23:49:45 -0700

    Layouts for the pages.
    Default Template:
    
        <!DOCTYPE html>
        <html>
          <head>
            <title>Sup</title>
            {{content_for_header}}
          </head>
          <body>
            {{content_for_layout}}
          </body>
        </html>

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -25,18 +25,34 @@ class PublicController < ActionController::Base
     # render text:"blah", layout:false
     return options[:text] if !options[:text].blank? && !options[:layout]
 
+    body = render_liquid_body(options)
+    render_liquid_layout(body)
+  end
+
+private
+  def render_liquid_body(options)
     template = get_template(options)
     return no_template_found(options) if template.blank?
 
     compiled_liquid = Liquid::Template.parse(template.source)
-    if Rails.env.development?
+    body = if Rails.env.development?
       compiled_liquid.render!(public_view_assigns)
     else
       compiled_liquid.render(public_view_assigns)
     end
   end
 
-private
+  def render_liquid_layout(body)
+    layout = @theme.templates.find_by_name("layouts/theme")
+    return body if layout.blank?
+    compiled_liquid = Liquid::Template.parse(layout.source)
+    if Rails.env.development?
+      compiled_liquid.render!(layout_view_assigns(body))
+    else
+      compiled_liquid.render(layout_view_assigns(body))
+    end
+  end
+
   def get_template(options)
     options[:prefixes].each do |prefix|
       template = @theme.templates.find_by_name("#{prefix.to_s}/#{options[:template]}")
@@ -53,5 +69,9 @@ private
 
   def public_view_assigns
     view_assigns.reject{|k,v| COMLOQUE_PROTECTED_IVARS.include?(k)}
+  end
+
+  def layout_view_assigns(body)
+    public_view_assigns.merge({'content_for_header'=>"",'content_for_layout'=>body})
   end
 end
