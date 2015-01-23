33eb51130145beb624343e01d135fda6d841cdad
Ryan Alyea <ryan@fangamer.com>
2015-01-23 02:08:23 -0700

    Helper func

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   LIQUEFIABLE_ATTRIBUTES = %i(name).freeze
   LIQUEFIABLE_METHODS = {}.freeze
   include Liquefiable
+
+  def is_admin?
+    superuser == 'administrator'
+  end
 end
