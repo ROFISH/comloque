46bcda771e57bdba30d0c7c2f029a5b145139aff
Ryan Alyea <ryan@fangamer.com>
2015-01-26 03:20:40 -0700

    Add a body_original to the liquid so as to see what the original was when editing

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,15 +1,21 @@
 class Message < ActiveRecord::Base
+  include ERB::Util
+
   belongs_to :user
   belongs_to :topic
 
   LIQUEFIABLE_ATTRIBUTES = %i(id).freeze
-  LIQUEFIABLE_METHODS = {user: :user, created_at: :created_at, updated_at: :updated_at, url: :url, body: :sanitized_body}.freeze
+  LIQUEFIABLE_METHODS = {user: :user, created_at: :created_at, updated_at: :updated_at, url: :url, body: :sanitized_body, body_original: :body_original}.freeze
   include Liquefiable
 
   include Sanitizable
 
   def url
     "#{topic.url}/#{id}"
+  end
+
+  def body_original
+    html_escape(body)
   end
 
   def sanitized_body
